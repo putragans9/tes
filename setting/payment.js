fb82ded23c10aef89d5caf69816f24e9fe0361d1
putragans9 <putragans970@gmail.com>
2022-02-10 14:49:46 +0700

    tes

---
+++ setting/payment.js
@@ -0,0 +1,9 @@
+exports.payment = (prefix) => {
+return `╭──「 *PAYMENT / DONASI* 」
+│
+├❏ Gopay : _Tuh Kak Scan Qrnya_
+├❏ Dana : _Tuh Kak Scan Qrnya__
+├❏ Ovo : _Tuh Kak Scan Qrnya_
+│
+╰──`
+}
---
+++ setting/sewabot.js
@@ -0,0 +1,15 @@
+exports.sewabot = (prefix) => {
+return `╭──「 *SEWABOT* 」
+│
+├❏ 1 JAM : Free
+├❏ Jadi Bot : 10K Permanen
+├❏ PERMANEN 1 GRUP : 5.000 ( 5K )
+│
+├──「 *PREMIUM* 」
+│
+├❏ JADI BOT+OWNER : 25K Tinggal Scen Qiris
+├❏ 1 BULAN : 5.000 ( 5K )
+├❏ PERMANEN : 10.000 ( 10K )
+│
+╰──「 *${botname}* 」`
+}
